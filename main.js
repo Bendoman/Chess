738e98a618b3ea2cb6831da7f4beafb3d9d80c67
Ben <Bendavcorr@Gmail.com>
2020-05-29 10:07:24 +0100

    Added tiles array grid

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const c = canvas.getContext('2d');
 
 const sprites = [];
 const pieces = [];
-const tiles = [];
+const tiles = [[],[],[],[],[],[],[],[]];
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight; 
@@ -38,7 +38,7 @@ for(let i = 0; i < 12; i++)
 
 //Create the board by filling the tiles array with Tile objects
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-'test';
+
 let x = 300;
 let y = 660; 
 
@@ -50,16 +50,16 @@ for(let i = 0; i < 8; i++)
         if(i % 2 == 0)
         {
             if(j % 2 == 0)
-                tiles.push(new Tile(x, y, '#BA5546', letters[j] + parseInt(i + 1)));
+                tiles[i].push(new Tile(x, y, '#BA5546', letters[j] + parseInt(i + 1)));
             else 
-                tiles.push(new Tile(x, y, '#F0D8BF', letters[j] + parseInt(i + 1)));
+                tiles[i].push(new Tile(x, y, '#F0D8BF', letters[j] + parseInt(i + 1)));
         }
         else 
         {
             if(j % 2 == 0)
-                tiles.push(new Tile(x, y, '#F0D8BF', letters[j] + parseInt(i + 1)));
+                tiles[i].push(new Tile(x, y, '#F0D8BF', letters[j] + parseInt(i + 1)));
             else 
-                tiles.push(new Tile(x, y, '#BA5546', letters[j] + parseInt(i + 1)));   
+                tiles[i].push(new Tile(x, y, '#BA5546', letters[j] + parseInt(i + 1)));   
         }
         x+=80;
     }
@@ -67,18 +67,21 @@ for(let i = 0; i < 8; i++)
 }
 //-------------------------------------------------------------
 
-pieces.push(new Piece(tiles[6], sprites[5]));
+pieces.push(new Piece(tiles[1][1], sprites[5]));
 
 
 function loop()
 {   
     //Draw each tile in the tiles array
-    for(let i = 0; i < tiles.length; i++)
+    for(let i = 0; i < 8; i++)
     {
-        c.fillStyle = tiles[i].colour;
-        c.fillRect(tiles[i].xPos, tiles[i].yPos, 80, 80);
-        c.fillStyle = 'black';
-        c.fillText(tiles[i].name, tiles[i].xPos + 2, tiles[i].yPos + 78, 50);
+        for(let y = 0; y < 8; y++)
+        {
+            c.fillStyle = tiles[i][y].colour;
+            c.fillRect(tiles[i][y].xPos, tiles[i][y].yPos, 80, 80);
+            c.fillStyle = 'black';
+            c.fillText(tiles[i][y].name, tiles[i][y].xPos + 2, tiles[i][y].yPos + 78, 50);
+        }
     }
     
     //Draw each piece in the pieces array
